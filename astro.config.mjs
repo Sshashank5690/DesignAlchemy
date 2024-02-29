31419ef6b9cba5143908480f68df1bd5bcb1cfba
Sshashank5690 <sshubham.5690@gmail.com>
2024-02-29 17:32:50 +0530

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), compress()],
+  webAnalytics: { enabled: true },
 });
