f8da14e6540f43acc0d060a09047dcbf4d8b384d
Sshashank5690 <sshubham.5690@gmail.com>
2024-02-29 17:45:21 +0530

    vercel revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-import vercel from '@astrojs/vercel/serverless';
-// Import Vercel Analytics adapter
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
-  integrations: [tailwind(), compress()], // Keep existing integrations
-  output: 'server', // Ensure output is set to 'server' for Vercel deployment
-  adapter: vercel({
-    // Add Vercel adapter configuration
-    webAnalytics: { enabled: true }, // Enable Vercel Analytics
-  }),
+  integrations: [tailwind(), compress()],
 });
